1cff7f23dab41406982d541855b66c5603384c12
Your Name <you@example.com>
2017-01-12 19:04:20 +0800

    use the right database configuration

--- config/db.php
+++ config/db.php
@@ -8,12 +8,12 @@ $dbConf = [
     'charset' => 'utf8',
 ];
 
-if (YII_DEBUG) {
+if (YII_ENV !== 'dev') {
 	$dbConf = [
 	    'class' => 'yii\db\Connection',
-	    'dsn' => 'mysql:host=localhost;dbname=mobile_carrier_detector',
-	    'username' => 'root',
-	    'password' => '',
+	    'dsn' => 'mysql:host=localhost;dbname=site8_mobilecarrier',
+	    'username' => 'site8_mobilecar',
+	    'password' => 'hitman052529',
 	    'charset' => 'utf8',
 	];
 }
