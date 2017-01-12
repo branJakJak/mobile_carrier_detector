f9902b3ce979141bed170d0db8d8497a2b4888ce
Your Name <you@example.com>
2017-01-13 00:48:38 +0800

    updated title

--- views/site/index.php
+++ views/site/index.php
@@ -5,7 +5,7 @@
 use yii\bootstrap\ActiveForm;
 use yii\bootstrap\Html;
 
-$this->title = \Yii::$app->name.' - Upload';
+$this->title = \Yii::$app->params['site_title'].' - Upload';
 ?>
 <div class="row">
     <div class="col-xs-3 col-sm-3 col-md-3 col-lg-3">
